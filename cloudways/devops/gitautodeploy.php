0828ee6bee308253b61db90056562ed489ebbd99
zackrspv <oldoak@gmail.com>
2022-10-12 23:04:25 -0700

    Create gitautodeploy.php
    Signed-off-by: zackrspv <oldoak@gmail.com>

---
+++ cloudways/devops/gitautodeploy.php
@@ -0,0 +1,247 @@
+<?php
+
+/* ----------------------------------------------------------------
+* CONFIGURATION SECTION
+* Author: CloudWays and Phillip Rumple
+* Date: 10/12/2022
+* License: MIT License
+* ----------------------------------------------------------------
+*/
+
+// The CloudWays API key
+const API_KEY = "li0k37LiWX3W0ZiTWuAPFPMzYOVmFN";
+
+// The CloudWays API URL, this usually doesn't change
+const API_URL = "https://api.cloudways.com/api/v1";
+
+// Your registered CloudWays Email Address (note this should be the account of the root owner)
+const EMAIL = "prumple@aws.somee.social";
+
+// The desired Git Repository URL
+const GIT_URL = "git@github.com:MortgageLabs/ML-Website.git";
+
+/* ----------------------------------------------------------------
+* NEEDED FILES
+* ----------------------------------------------------------------
+*  you will need to make an env.php that lives in the same directory
+* as this application.
+*
+* File: env.php
+* <?php
+*   define('GITHUB_WEBHOOOK_SECRET', 'WHATEVER_YOUR_SECRET_IS');
+* ?>
+* note, you should also make sure you have an .htaccess file in place
+* that denies access to this file to the public
+*
+* File .htaccess
+* <FilesMatch "(env.php)$">
+*	Order Allow,Deny
+*	Deny from all
+*</FilesMatch>
+*
+* ----------------------------------------------------------------
+*/
+
+// require the environment file for the secret
+require("env.php");
+
+// normalize that
+$hookSecret = GITHUB_WEBHOOK_SECRET;
+
+// handle errors
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new \ErrorException($message, 0, $severity, $file, $line);
+});
+
+// handle exceptions
+set_exception_handler(function ($e) {
+    header('HTTP/1.1 500 Internal Server Error');
+    echo "Error on line {$e->getLine()}: " . htmlSpecialChars($e->getMessage());
+    die();
+});
+
+// hold the raw post data
+$rawPost = NULL;
+
+// verify if the secret is set
+if ($hookSecret !== NULL) {
+    // verify if this comes from the github process
+    if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+        throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
+    } elseif (!extension_loaded('hash')) {
+        // verify if we have the hash extension loaded, which is critical
+        throw new \Exception("Missing 'hash' extension to check the secret code validity.");
+    }
+    // explode the signature
+    list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+
+    // check if we can decrypt the signature
+    if (!in_array($algo, hash_algos(), TRUE)) {
+        throw new \Exception("Hash algorithm '$algo' is not supported.");
+    }
+
+    // read the contents of the payload form github
+    $rawPost = file_get_contents('php://input');
+
+    // if the secrets don't match quit
+    if (!hash_equals($hash, hash_hmac($algo, $rawPost, $hookSecret))) {
+        throw new \Exception('Hook secret does not match.');
+    }
+}
+
+// if content type isn't set, then quit, because we don't know how to read the payload
+if (!isset($_SERVER['CONTENT_TYPE'])) {
+    throw new \Exception("Missing HTTP 'Content-Type' header.");
+} elseif (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
+    // if it's not a github event, quit
+    throw new \Exception("Missing HTTP 'X-Github-Event' header.");
+}
+
+// handle the events
+switch ($_SERVER['CONTENT_TYPE']) {
+    // if it's JSON
+    case 'application/json':
+        // set the JSON data
+        $json = $rawPost ?: file_get_contents('php://input');
+        break;
+    case 'application/x-www-form-urlencoded':
+        // set the form encoded data
+        $json = $_POST['payload'];
+        break;
+    default:
+        // we don't handle any other types, and Github doesn't send any other types
+        throw new \Exception("Unsupported content type: $_SERVER[CONTENT_TYPE]");
+}
+
+
+// decode the GIT response
+$payload = json_decode($json);
+
+// fetch the branch by getting rid of the ref/heads
+$branch = str_replace('refs/heads/', '', $payload->ref);
+
+echo "Branch in question: " . $branch;
+
+//Fetch Access Token
+$tokenResponse = callCloudwaysAPI(
+    'POST',
+    '/oauth/access_token',
+    null,
+    [
+        'email' => EMAIL,
+        'api_key' => API_KEY
+    ]
+);
+
+// decode the access token
+$accessToken = $tokenResponse->access_token;
+
+// get a list of servers for the CW account
+$servers = callCloudWaysAPI('GET', '/server', $accessToken);
+
+// PROCESS the github event
+switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
+    // handle github's keep alive event
+    case 'ping':
+        echo 'pong';
+        break;
+    // handle pushes to the branches
+    case 'push':
+        // because push events are important, we need to know if the branch
+        // we are being sent is a branch we can push too locally
+        // first we need to check our applications array for the proper ID
+        $appId = 0;
+        $serverId = 0;
+
+        // if the return from Cloudways isn't passing, then quit
+        if ($servers->status !== true) {
+            die("no servers!");
+        }
+
+        // parse for our app and server id
+        foreach ($servers->servers as $server) {
+            echo "\nlooking on server: " . $server->label;
+            $success = false;
+            foreach ($server->apps as $app) {
+                echo "\nlooking at app: " . $app->label;
+                if ($app->label == $branch) {
+                    $appId = $app->id;
+                    $serverId = $server->id;
+                    $success = true;
+                    break;
+                }
+            }
+            // if we found our app/server, then break and continue on
+            if ($success === true) {
+                break;
+            }
+        }
+
+        // do we have any apps and servers?
+        if ($serverId == 0 || $appId == 0) {
+            die("\nApp not found that matches our branch, skipping!");
+        } else {
+            echo "\nApp found that matches our branch, publishing! \nID: " . $appId . " On server: " . $serverId;
+        }
+
+        // now that we know our app and server Id we can push to it
+        $gitPullResponse = callCloudWaysAPI('POST', '/git/pull', $accessToken, [
+            'server_id' => $serverId,
+            'app_id' => $appId,
+            'git_url' => GIT_URL,
+            'branch_name' => $branch
+        ]);
+
+        // we'll die if the above doesn't parse properly anyway
+        // so now just return the response to github
+        echo "\n" . json_encode($gitPullResponse);
+        header('HTTP/1.0 200 OK');
+
+        break;
+    default:
+        // let github know we don't handle other requests currently
+        header('HTTP/1.0 404 Not Found');
+        die();
+}
+
+/**
+ * Use this function to interact with the CloudWays API
+ *
+ * @param mixed $method - GET/POST/DELETE etc
+ * @param mixed $url - the api endpoint to interact with
+ * @param mixed $accessToken - the access token
+ * @param array $post - any parameters to apply
+ *
+ * @return json - the response
+ *
+ */
+function callCloudwaysAPI($method, $url, $accessToken, $post = [])
+{
+    $baseURL = API_URL;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($ch, CURLOPT_URL, $baseURL . $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //Set Authorization Header
+    if ($accessToken) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $accessToken]);
+    }
+
+    //Set Post Parameters
+    $encoded = '';
+    if (count($post)) {
+        foreach ($post as $name => $value) {
+            $encoded .= urlencode($name) . '=' . urlencode($value) . '&';
+        }
+        $encoded = substr($encoded, 0, strlen($encoded) - 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded);
+        curl_setopt($ch, CURLOPT_POST, 1);
+    }
+    $output = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($httpcode != '200') {
+        die('An error occurred code: ' . $httpcode . ' output: ' . substr($output, 0, 10000));
+    }
+    curl_close($ch);
+    return json_decode($output);
+}
