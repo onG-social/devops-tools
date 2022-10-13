acf553786c3b74da061df7080c2d8ce7cdfa414f
zackrspv <oldoak@gmail.com>
2022-10-12 23:05:24 -0700

    Create env-example.php
    Signed-off-by: zackrspv <oldoak@gmail.com>

---
+++ cloudways/devops/env-example.php
@@ -0,0 +1,8 @@
+/* ------------------------------------
+* make sure you rename this to env.php
+* and place it in with the gitautodeploy.php script
+*/
+
+<?
+  define("GITHUB_WEBHOOK_SECRET", "SOMETHING_STRONG");
+?>
